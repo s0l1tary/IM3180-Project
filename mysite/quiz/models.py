6b3fa12ef13f1a7965653eb9f357b40f9db378ee
ww <leeweiwen2004@gmail.com>
2025-10-25 13:06:53 +0800

    content page but display pdf not working fkkk

--- mysite/quiz/models.py
+++ mysite/quiz/models.py
@@ -9,6 +9,7 @@ class Topic(models.Model):
     name = models.CharField(max_length=120, unique=True)
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True)
+    pdf_file = models.FileField(upload_to='topic_pdfs/', blank=True, null=True)
 
     def __str__(self):
         return self.name
