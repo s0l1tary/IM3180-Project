0e3cdecaae69eb1b8d28ef6b2756eb46ea0b7c46
Jemond Lee <61956405+thecpinsiders@users.noreply.github.com>
2025-11-01 23:42:57 +0800

    grades page modified to show user quiz session and added filtering of topics

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -40,6 +40,33 @@ def practice(request):
 @login_required
 def grades(request):
     queryset = UserTopicProgress.objects.all()
+
+    # Get all quiz sessions
+    quiz_sessions = QuizSession.objects.select_related('topic').all()
+
+    # Get unique topics for dropdown
+    topics = Topic.objects.all()
+
+    # Filter by topic if selected
+    selected_topic_id = request.GET.get('topic')
+    selected_topic = None
+    
+    if selected_topic_id:
+        try:
+            selected_topic = Topic.objects.get(id=selected_topic_id)
+            quiz_sessions = quiz_sessions.filter(topic=selected_topic)
+        except Topic.DoesNotExist:
+            # If topic doesn't exist, show all sessions
+            pass
+    
+
+    context = {
+        'quiz_sessions': quiz_sessions,
+        'topics': topics,
+        'selected_topic': selected_topic,
+    }
+    
+
     if request.user.is_authenticated:
         queryset = queryset.filter(user=request.user)
-    return render(request, 'main/grades.html', {'progress_list': queryset})
+    return render(request, 'main/grades.html', context)
